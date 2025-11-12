b425c31b0aa0c483d7dd256c8c944c3e28e7bb2f
ramez <ramez.elzoz74@gmail.com>
2025-11-12 15:58:13 +0200

    Finish the fkn section title

---
+++ src/components/atoms/SecHeading.jsx
@@ -0,0 +1,9 @@
+function SecHeading({ children }) {
+  return (
+    <h2 className="text-center text-4xl sm:text-5xl font-reqaa py-8">
+      {children}
+    </h2>
+  );
+}
+
+export default SecHeading;

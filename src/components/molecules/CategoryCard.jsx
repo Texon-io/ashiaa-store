3c1b25a3d8cdf7838a529d068811da55ccb99015
ramez <ramez.elzoz74@gmail.com>
2025-11-16 22:11:27 +0200

    Sorry for brain lag but also finished the categories section in this branch by mistake :)

---
+++ src/components/molecules/CategoryCard.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router";
+
+function CategoryCard({ imgPath, to, children }) {
+  return (
+    <div
+      id="card"
+      className={`bg-green-500 bg-[url('${imgPath}')] h-48 sm:h-56 lg:h-64 rounded-xl overflow-hidden shadow-md`}
+    >
+      <Link
+        to={to}
+        className="overlay bg-black/60 hover:bg-black/75 transition-all duration-300 ease-linear w-full h-full flex items-center justify-center"
+      >
+        <h4 className="font-reqaa text-secondary-text-light text-5xl shadow-2xl">
+          {children}
+        </h4>
+      </Link>
+    </div>
+  );
+}
+
+export default CategoryCard;

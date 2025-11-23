4951a76aa6376de014c17f636f1ff925a40323f3
ramez <ramez.elzoz74@gmail.com>
2025-11-23 13:43:07 +0200

    Feat: finished the best selling prodcuts sec

--- src/components/organisms/QuoteSec.jsx
+++ src/components/organisms/QuoteSec.jsx
@@ -1,6 +1,6 @@
 function QuoteSec() {
   return (
-    <section id="quote" className=" p-8 rounded-4xl shadow-md">
+    <section id="quote" className=" p-8  rounded-4xl shadow-md">
       <div className="container text-center backdrop-blur-xs bg-white/10 border border-white/30 shadow-xl rounded-3xl mx-auto px-6 sm:px-16 py-8 w-fit">
         <q className="font-reqaa text-4xl text-accent-dark-2">
           القراءة حياةٌ إضافية لمن لا يكتفون بحياتهم.

d219868d1a306e2a33bdb9f7321f187e5c8a2d25
ramez <ramez.elzoz74@gmail.com>
2025-11-25 13:49:29 +0200

    Fix: cart title clr & notification pos & category imgs

--- src/components/organisms/CartHeader.jsx
+++ src/components/organisms/CartHeader.jsx
@@ -4,7 +4,9 @@ function CartHeader() {
   const { toggleCart } = useCart();
   return (
     <div className="flex justify-between items-center mb-4 pb-4 border-b border-accent-dark">
-      <h2 className="text-3xl font-bold font-reqaa">سلة المشتريات</h2>
+      <h2 className="text-3xl font-bold font-reqaa text-accent-dark">
+        سلة المشتريات
+      </h2>
       <button onClick={toggleCart} className="text-xl font-bold cursor-pointer">
         ✕
       </button>

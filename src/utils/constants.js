60c23d4d9397388c2198285c2cebef73ff6afcb3
ramez <ramez.elzoz74@gmail.com>
2026-02-04 08:38:15 +0200

    ..

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
 import shoppingIcon from "/images/Shopping-bag.png";
 import menuIcon from "/images/Menu.png";
 import placeHolder from "/images/product-placeholder.png";
-import TiktokIcon from "/images/tiktok.png";
 import WhatsAppIcon from "/images/whatsapp.png";
 
-export { shoppingIcon, menuIcon, placeHolder, TiktokIcon, WhatsAppIcon };
+export { shoppingIcon, menuIcon, placeHolder, WhatsAppIcon };
